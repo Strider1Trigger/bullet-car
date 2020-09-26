fd4b03f8e3cf910ead77b2f30e27ad3f59d5c730
Strider1Trigger <70731975+Strider1Trigger@users.noreply.github.com>
2020-09-26 15:33:12 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var bullet,wall, thickness;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(223,321)
+  weight=random(30,52)
+  thickness=random(22,83)
+  car=createSprite(50, 200, 10, 100);
+  wall=createSprite(1200,200,tickness,height/2);
+  wall=color(80,80,80);
+  car.velocityX=speed;
+}
+
+function draw() {
+  background(255,255,255);  
+
+  drawSprites
+ 
+
+}
